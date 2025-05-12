2f7f9fcdb5588e1c82d353ba118b28196c9e7c76
Jonathan Centurion <jonathan.centurion@teracode.com>
2025-05-12 18:48:11 -0300

    added cron schedule

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,10 +1,19 @@
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery(
     "late_checkout_tasks",
     broker="redis://redis:6379/0",
     backend="redis://redis:6379/0"
 )
+
+
+app.conf.beat_schedule = {
+    "send-daily-checkout-sms": {
+        "task": "schedule_today_checkouts",
+        "schedule": crontab(minute='*/1'),  # crontab(hour=16, minute=40)  todos los días a las 6am UTC (3am ARG)
+    }
+}
 
 app.conf.timezone = "UTC"
 app.conf.enable_utc = True
--- tasks.py
+++ tasks.py
@@ -1,6 +1,47 @@
+import logging
 from celeryconfig import app
 from send_message import send_sms
 from datetime import datetime
+from pytz import timezone, UTC
+
+logger = logging.getLogger(__name__)
+
+@app.task(name="schedule_today_checkouts")
+def schedule_today_checkouts():
+    ## esta funcion es llamada automaticamente por el contenedor de Celery beat
+    # y mediante el decorador ejecuta la configuracion de periodicidad del cron
+    
+    # Seccion de consulta a la BD###############
+    # extrae diariamente todos los huespedes que correspondan hacer el checkout
+    #today = datetime.now(pytz.timezone("America/Argentina/Buenos_Aires")).date()
+    #guests = db_query_checkout_today(today)  # <- reemplazá esto por tu consulta real 
+    
+    logger.warning(f"[CRON] ejecución del cron a las : {datetime.now()}")
+
+    # Mock de guests
+    guests = [
+    {"phone_number": "+541112345678", "guest_name": "Juan", "checkout_date": "2025-05-09"},
+    {"phone_number": "+541198765432", "guest_name": "Lucía", "checkout_date": "2025-05-09"},
+    # ...
+    ]
+
+    for guest in guests:
+        
+        checkout_str = guest.get("checkout_date") 
+        checkout_dt = datetime.strptime(checkout_str, "%Y-%m-%d")
+        
+        ba = timezone("America/Argentina/Buenos_Aires")
+        ba_dt = ba.localize(checkout_dt.replace(hour=16, minute=45, second=0))
+
+        # Convertir a UTC
+        scheduled_time = ba_dt.astimezone(UTC)
+
+        # Programar SMS
+        send_checkout_sms.apply_async(
+            args=[guest.get("phone_number"), guest.get("guest_name")],
+            eta=scheduled_time
+        )
+
 
 @app.task
 def send_checkout_sms(phone_number: str, guest_name: str):
